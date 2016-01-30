f948de273bbeff80bcc561724f5ab8bfd1ef1a83
alprusa <aprusa@ucsc.edu>
2016-01-29 21:19:06 -0800

    Start sequence check

---
+++ Assets/Scripts/SequenceChecker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SequenceChecker : MonoBehaviour {
+
+	public SequenceObjects sequenceObjects;
+
+	private int currentIndex;
+
+	// Use this for initialization
+	void Start () {
+		sequenceObjects = new SequenceObjects ();
+		currentIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void GetPlayersSequence(Department val){
+		if (sequenceObjects [currentIndex] != val)
+			Debug.Log ("Fail");
+		
+		currentIndex++;
+	}
+}
--- Assets/Scripts/SequenceObjects.cs
+++ Assets/Scripts/SequenceObjects.cs
@@ -1,9 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class SequenceObjects : MonoBehaviour {
+public class SequenceObjects {
 
 	public Department department;
 	public string value;
 	public int amount;
+
+	public List<Department> sequence;
+
+	public int sequenceLength = 10;
+
+	// Use this for initialization
+	public SequenceObjects() {
+		sequence = new List<Department> ();
+
+		for (int i = 0; i < sequenceLength; i++) {
+			switch (Random.Range (0.0f, 2.0f)) {
+			case 0.0f:
+				sequence [i] = Department.A;
+				break;
+			case 1.0f:
+				sequence[i] = Department.B;
+				break;
+			case 2.0f:
+				sequence [i] = Department.C;
+				break;
+			}
+		}
+	}
+
+	//function for failer?
+	//
 }

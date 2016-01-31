e184732d943954bb127a103794dc0917af8f634b
Kory <kfeath@ucsc.edu>
2016-01-30 20:18:37 -0800

    Shop

--- Assets/Scripts/GameMainController.cs
+++ Assets/Scripts/GameMainController.cs
@@ -1,8 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameMainController : MonoBehaviour {
+	private int counter=0;
+	public GameObject Shop;
+	public Text custom;
+	public void Shopping(){
+		Shop.SetActive (true);
+		counter++;
+		custom.text = "Exit";
+				if (counter > 2) {
+			custom.text = "Customize";
+			Shop.SetActive (false);
+			counter = 0;
+		}
 
+	}
 	public CanvasGroup CanvasGroup;
 	
 	public void Start() {
@@ -17,4 +31,5 @@ public class GameMainController : MonoBehaviour {
 		CanvasGroup.interactable = flag;
 		CanvasGroup.blocksRaycasts = flag;
 	}
+
 }
---
+++ Assets/Scripts/shop.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class shop : MonoBehaviour {
+	//public Button b1, b2, b3, b4;
+	//private Button storebutton;
+	public GameObject apple;
+
+	public void chosenimage(Button b){
+		apple.GetComponent<apple> ().saveimage = b.GetComponent<Image> ().sprite;
+		gameObject.SetActive (false);
+	}
+	public void removeimage(){
+		apple.GetComponent<apple> ().saveimage = null;
+		gameObject.SetActive (false);
+	}
+}

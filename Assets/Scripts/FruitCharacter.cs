a9d9da2bafcb8b867e753738434cee8272bd367e
Jonathan Lu <jonathanlu@dhcp-169-233-177-106.cruznet.ucsc.edu>
2016-01-30 18:07:00 -0800

    Spawning of apples working

--- Assets/Scripts/FruitCharacter.cs
+++ Assets/Scripts/FruitCharacter.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum Department {
-	A,
-	B,
-	C
+public enum FruitQuality {
+	Good,
+	Neutral,
+	Bad
 }
 
 public class FruitCharacter : MonoBehaviour {
 
-	public Department Department;
+	// GUI
+	public TextMesh NameTextMesh;
+
+	public FruitQuality Quality;
+	
+	private FruitModel cachedModel;
 
 	// Use this for initialization
 	void Start () {
 		
+	}
+	
+	public void InitWithModel(FruitModel model) {
+		cachedModel = model;
+		
+		if(model.Hours >= 15) {
+			Quality = FruitQuality.Good;
+		} else if(model.Hours >= 5) {
+			Quality = FruitQuality.Neutral;
+		} else {
+			Quality = FruitQuality.Bad;
+		}
+		
+		NameTextMesh.text = cachedModel.Name;
 	}
 	
 	#region Event Listeners
---
+++ Assets/Scripts/FruitGenerator.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FruitGenerator : MonoBehaviour {
+	public Transform FruitParent;
+	public FruitCharacter FruitPrefab;
+	public LevelLimits Limits;
+	
+	private List<FruitModel> FruitModels = new List<FruitModel>();
+	private List<FruitCharacter> Fruits = new List<FruitCharacter>();
+
+	// Use this for initialization
+	void Start () {
+		List<string> names = new List<string>() {
+			"Hurley",
+			"Sayid",
+			"Sawyer",
+			"Desmond",
+			"Kate",
+			"Jack",
+			"Walt",
+			"John",
+			"Boone"
+		};
+		
+		for(int i = 0; i < 100; i++) {
+			int randIndex = Random.Range(0, names.Count);
+			string randomString = names[ randIndex ];
+			
+			FruitModel fruitModel = new FruitModel();
+			fruitModel.Name = randomString;
+			fruitModel.Hours = Random.Range(5, 15);
+			FruitModels.Add(fruitModel);
+		}
+		
+		StartCoroutine(SpawnFruits());
+	}
+	
+	private IEnumerator SpawnFruits() {
+		float safeDistance = 6;
+		foreach(FruitModel model in FruitModels) {
+			float randomX = Random.Range (Limits.LeftLimit, Limits.RightLimit);
+			float randomY = Random.Range (Limits.BottomLimit, Limits.TopLimit);
+			Vector3 spawnPosition = new Vector3(randomX, randomY, 0);
+			bool foundValidLocation = false;
+			int attempts = 0;
+			while(!foundValidLocation && attempts < 100) {
+				foundValidLocation = true;
+				foreach(FruitCharacter character in Fruits) {
+					float dist = Vector2.Distance(character.transform.position, spawnPosition);
+					if(dist <= safeDistance) {
+						randomX = Random.Range (Limits.LeftLimit, Limits.RightLimit);
+						randomY = Random.Range (Limits.BottomLimit, Limits.TopLimit);
+						spawnPosition = new Vector3(randomX, randomY, 0);
+						foundValidLocation = false;
+						break;
+					}
+				}
+				
+				attempts++;
+				
+				yield return null;
+			}
+			
+			FruitCharacter fruitCharacter = (FruitCharacter) Instantiate(FruitPrefab, spawnPosition, Quaternion.identity);
+			fruitCharacter.InitWithModel(model);
+			fruitCharacter.transform.SetParent(FruitParent);
+			Fruits.Add(fruitCharacter);
+			
+			yield return null;
+		}
+	}
+}
